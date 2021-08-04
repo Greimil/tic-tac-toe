f88f6179ac40980deffcc3c45fc3f8495f586bb5
Greimil <greimilnunez@gmail.com>
2021-08-04 09:24:47 -0400

    diagonal bug fixed

--- app.js
+++ app.js
@@ -1,97 +1,89 @@
-const cellsElements = document.querySelectorAll("[data-cell]")
-const Board = document.getElementById("board")
-const CICLE_CLASS = "circle"
-const X_CLASS = "x"
-const restarBtn = document.getElementById("restartButton")
-const winningMessageElement = document.getElementById("winningMessage")
-const winningMessageTextElement = document.querySelector("[data-winning-message-text]")
-const winning_combination = [
-    [0,1,2],
-    [3,4,6],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,6,8],
-    [0,4,9],
-    [2,4,6]
-
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
 ]
-let circleTurn 
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+const winningMessageElement = document.getElementById('winningMessage')
+const restartButton = document.getElementById('restartButton')
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+let circleTurn
 
+startGame()
 
-starGame()
+restartButton.addEventListener('click', startGame)
 
-
-function starGame(){
-    circleTurn = false
-    cellsElements.forEach(cell => {
-        cell.classList.remove(X_CLASS)
-        cell.classList.remove(CICLE_CLASS)
-        cell.removeAttribute("click",handleClick)
-        cell.addEventListener("click", handleClick,{once: true})
-    })
-    setBoardHoverClass()
-    winningMessageElement.classList.remove("show")
+function startGame() {
+  circleTurn = false
+  cellElements.forEach(cell => {
+    cell.classList.remove(X_CLASS)
+    cell.classList.remove(CIRCLE_CLASS)
+    cell.removeEventListener('click', handleClick)
+    cell.addEventListener('click', handleClick, { once: true })
+  })
+  setBoardHoverClass()
+  winningMessageElement.classList.remove('show')
 }
-
-restarBtn.addEventListener("click",starGame)
 
 function handleClick(e) {
-    const cell = e.target
-    const currentClass = circleTurn ? CICLE_CLASS: X_CLASS
-    placeMark(cell,currentClass)
-    if(checkWin(currentClass)){
-       endGame(false)
-    }  else if(isDraw()){
-        endGame(true)
-    } else{
-        swapTurns()
-        setBoardHoverClass()
-    }
+  const cell = e.target
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+  placeMark(cell, currentClass)
+  if (checkWin(currentClass)) {
+    endGame(false)
+  } else if (isDraw()) {
+    endGame(true)
+  } else {
+    swapTurns()
+    setBoardHoverClass()
+  }
 }
 
-function endGame(draw){
-    if(draw){
-        winningMessageTextElement.innerHTML = "Draw!"
-    } else {
-        winningMessageTextElement.innerHTML = `${circleTurn ? "O's": "X's"} Wins!`
-    }
-    winningMessageElement.classList.add("show")
+function endGame(draw) {
+  if (draw) {
+    winningMessageTextElement.innerText = 'Draw!'
+  } else {
+    winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+  }
+  winningMessageElement.classList.add('show')
 }
 
-
-function isDraw(){
-    return [...cellsElements].every(cell =>{
-        return cell.classList.contains(X_CLASS) || cell.classList.contains(CICLE_CLASS)
-    })
+function isDraw() {
+  return [...cellElements].every(cell => {
+    return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+  })
 }
 
-
-function placeMark(cell,currentClass){
-    cell.classList.add(currentClass)
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass)
 }
 
-function swapTurns(){
-    circleTurn = !circleTurn
+function swapTurns() {
+  circleTurn = !circleTurn
 }
 
-function setBoardHoverClass(){
-    Board.classList.remove(CICLE_CLASS)
-    Board.classList.remove(X_CLASS)
-    if(circleTurn){
-        Board.classList.add(CICLE_CLASS)
-    } else{
-        Board.classList.add(X_CLASS)
-    }
+function setBoardHoverClass() {
+  board.classList.remove(X_CLASS)
+  board.classList.remove(CIRCLE_CLASS)
+  if (circleTurn) {
+    board.classList.add(CIRCLE_CLASS)
+  } else {
+    board.classList.add(X_CLASS)
+  }
 }
 
 function checkWin(currentClass) {
-    return winning_combination.some(combination => {
-      return combination.every(index => {
-        return cellsElements[index].classList.contains(currentClass)
-      })
+  return WINNING_COMBINATIONS.some(combination => {
+    return combination.every(index => {
+      return cellElements[index].classList.contains(currentClass)
     })
-  }
-
-
-  restarBtn.addEventListener("click",starGame)
+  })
+}
